94d70e552179d270e19baec663cb9b330f62e80e
吃草莓的小屋 <62467038+chrelyonly@users.noreply.github.com>
2023-04-17 17:14:53 +0800

    Update layout.tsx
    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "🍓",
+  description: "🍓.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "🍓",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
